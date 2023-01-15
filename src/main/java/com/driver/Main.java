ff47950917347be7ec347bb82ae686c3a914c85a
Mr-Vivek-8bit <viveksonu444@gmail.com>
2023-01-15 18:46:01 +0530

    solution two

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,12 +2,12 @@ package com.driver;
 
 public class Main {
 
-    static class Product{
+   public static class Product{
           int x = 0, y = 0, z = 0;
         public int product(int x, int y) {
             this.x = x;
             this.y = y;
-            return x+y;
+            return x*y;
         }
 
         public int product(int x, int y, int z) {
@@ -15,14 +15,14 @@ public class Main {
             this.y = y;
             this.z = z;
 
-            return x+y+z;
+            return x*y*z;
         }
 
         public double product(double x, double y) {
             x = x;
             y = y;
 
-            return x+y;
+            return x*y;
         }
 
 
